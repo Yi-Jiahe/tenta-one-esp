a899ed3018c4d05ac87a450e8ee24a4f348d4bf2
Yi Jiahe <yi.jiahe00@gmail.com>
2022-09-07 16:09:32 +0800

    Add encoder component

--- main/main.c
+++ main/main.c
@@ -4,9 +4,11 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 
+#include "rotary_encoder.h"
+
 static const int kEncoderSwitchPin = 25;
-// static const int kEncoderA = 33;
-// static const int kEncoderB = 32;
+static const int kEncoderA = 33;
+static const int kEncoderB = 32;
 
 static const char *TAG = "main";
 
@@ -32,9 +34,32 @@ static void configure_gpio(void)
     gpio_set_pull_mode(kEncoderSwitchPin, GPIO_PULLUP_ONLY);
 }
 
+static void encoder_counter_task(void *pvParameter)
+{
+    // Rotary encoder underlying device is represented by a PCNT unit in this example
+    uint32_t pcnt_unit = 0;
+
+    // Create rotary encoder instance
+    rotary_encoder_config_t config = ROTARY_ENCODER_DEFAULT_CONFIG((rotary_encoder_dev_t)pcnt_unit, kEncoderA, kEncoderB);
+    rotary_encoder_t *encoder = NULL;
+    ESP_ERROR_CHECK(rotary_encoder_new_ec11(&config, &encoder));
+
+    // Filter out glitch (1us)
+    ESP_ERROR_CHECK(encoder->set_glitch_filter(encoder, 1));
+
+    // Start encoder
+    ESP_ERROR_CHECK(encoder->start(encoder));
+
+    while (1) {
+        ESP_LOGI(TAG, "Encoder value: %d", encoder->get_counter_value(encoder));
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
 void app_main(void)
 {
     configure_gpio();
 
     xTaskCreate(&encoder_switch_reading_task, "button_task", 2048, NULL, 10, NULL);
+    xTaskCreate(&encoder_counter_task, "encoder_task", 2048, NULL, 10, NULL);
 }
