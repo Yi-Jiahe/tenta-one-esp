8c74ee2f21d8a2904769d21c093826a249a0da8f
Yi Jiahe <yi.jiahe00@gmail.com>
2022-09-08 13:07:19 +0800

    move protocol defintions out of main

--- main/main.c
+++ main/main.c
@@ -26,6 +26,8 @@
 
 #include "esp_hidd.h"
 #include "esp_hid_gap.h"
+#include "report_maps.h"
+#include "hid_cc_report.h"
 
 static const int kEncoderSwitchPin = 25;
 static const int kEncoderA = 33;
@@ -178,87 +180,6 @@ static void joystick_read_task(void *pvParameter)
 
 #if CONFIG_BT_BLE_ENABLED
 static local_param_t s_ble_hid_param = {0};
-const unsigned char hidapiReportMap[] = { //8 bytes input, 8 bytes feature
-    0x06, 0x00, 0xFF,  // Usage Page (Vendor Defined 0xFF00)
-    0x0A, 0x00, 0x01,  // Usage (0x0100)
-    0xA1, 0x01,        // Collection (Application)
-    0x85, 0x01,        //   Report ID (1)
-    0x15, 0x00,        //   Logical Minimum (0)
-    0x26, 0xFF, 0x00,  //   Logical Maximum (255)
-    0x75, 0x08,        //   Report Size (8)
-    0x95, 0x08,        //   Report Count (8)
-    0x09, 0x01,        //   Usage (0x01)
-    0x82, 0x02, 0x01,  //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Buffered Bytes)
-    0x95, 0x08,        //   Report Count (8)
-    0x09, 0x02,        //   Usage (0x02)
-    0xB2, 0x02, 0x01,  //   Feature (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile,Buffered Bytes)
-    0x95, 0x08,        //   Report Count (8)
-    0x09, 0x03,        //   Usage (0x03)
-    0x91, 0x02,        //   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    0xC0,              // End Collection
-
-    // 38 bytes
-};
-
-const unsigned char mediaReportMap[] = {
-    0x05, 0x0C,        // Usage Page (Consumer)
-    0x09, 0x01,        // Usage (Consumer Control)
-    0xA1, 0x01,        // Collection (Application)
-    0x85, 0x03,        //   Report ID (3)
-    0x09, 0x02,        //   Usage (Numeric Key Pad)
-    0xA1, 0x02,        //   Collection (Logical)
-    0x05, 0x09,        //     Usage Page (Button)
-    0x19, 0x01,        //     Usage Minimum (0x01)
-    0x29, 0x0A,        //     Usage Maximum (0x0A)
-    0x15, 0x01,        //     Logical Minimum (1)
-    0x25, 0x0A,        //     Logical Maximum (10)
-    0x75, 0x04,        //     Report Size (4)
-    0x95, 0x01,        //     Report Count (1)
-    0x81, 0x00,        //     Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0,              //   End Collection
-    0x05, 0x0C,        //   Usage Page (Consumer)
-    0x09, 0x86,        //   Usage (Channel)
-    0x15, 0xFF,        //   Logical Minimum (-1)
-    0x25, 0x01,        //   Logical Maximum (1)
-    0x75, 0x02,        //   Report Size (2)
-    0x95, 0x01,        //   Report Count (1)
-    0x81, 0x46,        //   Input (Data,Var,Rel,No Wrap,Linear,Preferred State,Null State)
-    0x09, 0xE9,        //   Usage (Volume Increment)
-    0x09, 0xEA,        //   Usage (Volume Decrement)
-    0x15, 0x00,        //   Logical Minimum (0)
-    0x75, 0x01,        //   Report Size (1)
-    0x95, 0x02,        //   Report Count (2)
-    0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x09, 0xE2,        //   Usage (Mute)
-    0x09, 0x30,        //   Usage (Power)
-    0x09, 0x83,        //   Usage (Recall Last)
-    0x09, 0x81,        //   Usage (Assign Selection)
-    0x09, 0xB0,        //   Usage (Play)
-    0x09, 0xB1,        //   Usage (Pause)
-    0x09, 0xB2,        //   Usage (Record)
-    0x09, 0xB3,        //   Usage (Fast Forward)
-    0x09, 0xB4,        //   Usage (Rewind)
-    0x09, 0xB5,        //   Usage (Scan Next Track)
-    0x09, 0xB6,        //   Usage (Scan Previous Track)
-    0x09, 0xB7,        //   Usage (Stop)
-    0x15, 0x01,        //   Logical Minimum (1)
-    0x25, 0x0C,        //   Logical Maximum (12)
-    0x75, 0x04,        //   Report Size (4)
-    0x95, 0x01,        //   Report Count (1)
-    0x81, 0x00,        //   Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x09, 0x80,        //   Usage (Selection)
-    0xA1, 0x02,        //   Collection (Logical)
-    0x05, 0x09,        //     Usage Page (Button)
-    0x19, 0x01,        //     Usage Minimum (0x01)
-    0x29, 0x03,        //     Usage Maximum (0x03)
-    0x15, 0x01,        //     Logical Minimum (1)
-    0x25, 0x03,        //     Logical Maximum (3)
-    0x75, 0x02,        //     Report Size (2)
-    0x81, 0x00,        //     Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0,              //   End Collection
-    0x81, 0x03,        //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0,              // End Collection
-};
 
 static esp_hid_raw_report_map_t ble_report_maps[] = {
     {
@@ -275,160 +196,17 @@ static esp_hid_device_config_t ble_hid_config = {
     .vendor_id          = 0x16C0,
     .product_id         = 0x05DF,
     .version            = 0x0100,
-    .device_name        = "ESP BLE HID2",
+    .device_name        = "Tenta-one BLE",
     .manufacturer_name  = "Espressif",
     .serial_number      = "1234567890",
     .report_maps        = ble_report_maps,
     .report_maps_len    = 2
 };
 
-#define HID_CC_RPT_MUTE                 1
-#define HID_CC_RPT_POWER                2
-#define HID_CC_RPT_LAST                 3
-#define HID_CC_RPT_ASSIGN_SEL           4
-#define HID_CC_RPT_PLAY                 5
-#define HID_CC_RPT_PAUSE                6
-#define HID_CC_RPT_RECORD               7
-#define HID_CC_RPT_FAST_FWD             8
-#define HID_CC_RPT_REWIND               9
-#define HID_CC_RPT_SCAN_NEXT_TRK        10
-#define HID_CC_RPT_SCAN_PREV_TRK        11
-#define HID_CC_RPT_STOP                 12
-
-#define HID_CC_RPT_CHANNEL_UP           0x10
-#define HID_CC_RPT_CHANNEL_DOWN         0x30
-#define HID_CC_RPT_VOLUME_UP            0x40
-#define HID_CC_RPT_VOLUME_DOWN          0x80
-
-// HID Consumer Control report bitmasks
-#define HID_CC_RPT_NUMERIC_BITS         0xF0
-#define HID_CC_RPT_CHANNEL_BITS         0xCF
-#define HID_CC_RPT_VOLUME_BITS          0x3F
-#define HID_CC_RPT_BUTTON_BITS          0xF0
-#define HID_CC_RPT_SELECTION_BITS       0xCF
-
-// Macros for the HID Consumer Control 2-byte report
-#define HID_CC_RPT_SET_NUMERIC(s, x)    (s)[0] &= HID_CC_RPT_NUMERIC_BITS;   (s)[0] = (x)
-#define HID_CC_RPT_SET_CHANNEL(s, x)    (s)[0] &= HID_CC_RPT_CHANNEL_BITS;   (s)[0] |= ((x) & 0x03) << 4
-#define HID_CC_RPT_SET_VOLUME_UP(s)     (s)[0] &= HID_CC_RPT_VOLUME_BITS;    (s)[0] |= 0x40
-#define HID_CC_RPT_SET_VOLUME_DOWN(s)   (s)[0] &= HID_CC_RPT_VOLUME_BITS;    (s)[0] |= 0x80
-#define HID_CC_RPT_SET_BUTTON(s, x)     (s)[1] &= HID_CC_RPT_BUTTON_BITS;    (s)[1] |= (x)
-#define HID_CC_RPT_SET_SELECTION(s, x)  (s)[1] &= HID_CC_RPT_SELECTION_BITS; (s)[1] |= ((x) & 0x03) << 4
-
-// HID Consumer Usage IDs (subset of the codes available in the USB HID Usage Tables spec)
-#define HID_CONSUMER_POWER          48  // Power
-#define HID_CONSUMER_RESET          49  // Reset
-#define HID_CONSUMER_SLEEP          50  // Sleep
-
-#define HID_CONSUMER_MENU           64  // Menu
-#define HID_CONSUMER_SELECTION      128 // Selection
-#define HID_CONSUMER_ASSIGN_SEL     129 // Assign Selection
-#define HID_CONSUMER_MODE_STEP      130 // Mode Step
-#define HID_CONSUMER_RECALL_LAST    131 // Recall Last
-#define HID_CONSUMER_QUIT           148 // Quit
-#define HID_CONSUMER_HELP           149 // Help
-#define HID_CONSUMER_CHANNEL_UP     156 // Channel Increment
-#define HID_CONSUMER_CHANNEL_DOWN   157 // Channel Decrement
-
-#define HID_CONSUMER_PLAY           176 // Play
-#define HID_CONSUMER_PAUSE          177 // Pause
-#define HID_CONSUMER_RECORD         178 // Record
-#define HID_CONSUMER_FAST_FORWARD   179 // Fast Forward
-#define HID_CONSUMER_REWIND         180 // Rewind
-#define HID_CONSUMER_SCAN_NEXT_TRK  181 // Scan Next Track
-#define HID_CONSUMER_SCAN_PREV_TRK  182 // Scan Previous Track
-#define HID_CONSUMER_STOP           183 // Stop
-#define HID_CONSUMER_EJECT          184 // Eject
-#define HID_CONSUMER_RANDOM_PLAY    185 // Random Play
-#define HID_CONSUMER_SELECT_DISC    186 // Select Disk
-#define HID_CONSUMER_ENTER_DISC     187 // Enter Disc
-#define HID_CONSUMER_REPEAT         188 // Repeat
-#define HID_CONSUMER_STOP_EJECT     204 // Stop/Eject
-#define HID_CONSUMER_PLAY_PAUSE     205 // Play/Pause
-#define HID_CONSUMER_PLAY_SKIP      206 // Play/Skip
-
-#define HID_CONSUMER_VOLUME         224 // Volume
-#define HID_CONSUMER_BALANCE        225 // Balance
-#define HID_CONSUMER_MUTE           226 // Mute
-#define HID_CONSUMER_BASS           227 // Bass
-#define HID_CONSUMER_VOLUME_UP      233 // Volume Increment
-#define HID_CONSUMER_VOLUME_DOWN    234 // Volume Decrement
-
-#define HID_RPT_ID_CC_IN        3   // Consumer Control input report ID
-#define HID_CC_IN_RPT_LEN       2   // Consumer Control input report Len
 void esp_hidd_send_consumer_value(uint8_t key_cmd, bool key_pressed)
 {
     uint8_t buffer[HID_CC_IN_RPT_LEN] = {0, 0};
-    if (key_pressed) {
-        switch (key_cmd) {
-        case HID_CONSUMER_CHANNEL_UP:
-            HID_CC_RPT_SET_CHANNEL(buffer, HID_CC_RPT_CHANNEL_UP);
-            break;
-
-        case HID_CONSUMER_CHANNEL_DOWN:
-            HID_CC_RPT_SET_CHANNEL(buffer, HID_CC_RPT_CHANNEL_DOWN);
-            break;
-
-        case HID_CONSUMER_VOLUME_UP:
-            HID_CC_RPT_SET_VOLUME_UP(buffer);
-            break;
-
-        case HID_CONSUMER_VOLUME_DOWN:
-            HID_CC_RPT_SET_VOLUME_DOWN(buffer);
-            break;
-
-        case HID_CONSUMER_MUTE:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_MUTE);
-            break;
-
-        case HID_CONSUMER_POWER:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_POWER);
-            break;
-
-        case HID_CONSUMER_RECALL_LAST:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_LAST);
-            break;
-
-        case HID_CONSUMER_ASSIGN_SEL:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_ASSIGN_SEL);
-            break;
-
-        case HID_CONSUMER_PLAY:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_PLAY);
-            break;
-
-        case HID_CONSUMER_PAUSE:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_PAUSE);
-            break;
-
-        case HID_CONSUMER_RECORD:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_RECORD);
-            break;
-
-        case HID_CONSUMER_FAST_FORWARD:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_FAST_FWD);
-            break;
-
-        case HID_CONSUMER_REWIND:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_REWIND);
-            break;
-
-        case HID_CONSUMER_SCAN_NEXT_TRK:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_SCAN_NEXT_TRK);
-            break;
-
-        case HID_CONSUMER_SCAN_PREV_TRK:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_SCAN_PREV_TRK);
-            break;
-
-        case HID_CONSUMER_STOP:
-            HID_CC_RPT_SET_BUTTON(buffer, HID_CC_RPT_STOP);
-            break;
-
-        default:
-            break;
-        }
-    }
+    esp_hidd_consumer_value_buffer(buffer, key_cmd, key_pressed);
     esp_hidd_dev_input_set(s_ble_hid_param.hid_dev, 1, HID_RPT_ID_CC_IN, buffer, HID_CC_IN_RPT_LEN);
     return;
 }
@@ -520,39 +298,6 @@ static void ble_hidd_event_callback(void *handler_args, esp_event_base_t base, i
 
 #if CONFIG_BT_HID_DEVICE_ENABLED
 static local_param_t s_bt_hid_param = {0};
-const unsigned char mouseReportMap[] = {
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    0x09, 0x02,                    // USAGE (Mouse)
-    0xa1, 0x01,                    // COLLECTION (Application)
-
-    0x09, 0x01,                    //   USAGE (Pointer)
-    0xa1, 0x00,                    //   COLLECTION (Physical)
-
-    0x05, 0x09,                    //     USAGE_PAGE (Button)
-    0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
-    0x29, 0x03,                    //     USAGE_MAXIMUM (Button 3)
-    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
-    0x95, 0x03,                    //     REPORT_COUNT (3)
-    0x75, 0x01,                    //     REPORT_SIZE (1)
-    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
-    0x95, 0x01,                    //     REPORT_COUNT (1)
-    0x75, 0x05,                    //     REPORT_SIZE (5)
-    0x81, 0x03,                    //     INPUT (Cnst,Var,Abs)
-
-    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
-    0x09, 0x30,                    //     USAGE (X)
-    0x09, 0x31,                    //     USAGE (Y)
-    0x09, 0x38,                    //     USAGE (Wheel)
-    0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
-    0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
-    0x75, 0x08,                    //     REPORT_SIZE (8)
-    0x95, 0x03,                    //     REPORT_COUNT (3)
-    0x81, 0x06,                    //     INPUT (Data,Var,Rel)
-
-    0xc0,                          //   END_COLLECTION
-    0xc0                           // END_COLLECTION
-};
 
 static esp_hid_raw_report_map_t bt_report_maps[] = {
     {
@@ -565,7 +310,7 @@ static esp_hid_device_config_t bt_hid_config = {
     .vendor_id          = 0x16C0,
     .product_id         = 0x05DF,
     .version            = 0x0100,
-    .device_name        = "Tenta-one",
+    .device_name        = "Tenta-one BT Classic",
     .manufacturer_name  = "Espressif",
     .serial_number      = "1234567890",
     .report_maps        = bt_report_maps,
